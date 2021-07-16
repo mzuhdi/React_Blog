bb64f5061fea81991cdfb2ed06ecb79dc0e136e2
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-07-16 16:34:48 -0400

    added useEffect hook

---
+++ src/BlogList.js
@@ -0,0 +1,22 @@
+const BlogList = ({blogs, title, handleDelete}) => {
+
+    //Props: passing down info from parents to child components, can bypass doing this by 
+    //just passing it like an argument as shown above
+    //const blogs = props.blogs;
+    //const title = props.title;
+    
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                        <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+                    </div>
+                ))}
+        </div>
+    )
+}
+
+export default BlogList
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import BlogList from "./BlogList";
 
 const Home = () => {
 
     //useState hook syntax. Making a variable reactive
-    const[name, setName] = useState("mario")
+    const [name, setName] = useState("mario");
+    const [blogs, setBlogs] = useState(
+        [
+            { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        ]
+    );
 
     const hello = () => {
         console.log("hello from console");
@@ -11,8 +19,18 @@ const Home = () => {
     }
 
     const helloinput = (name) => {
-        console.log ("hello " + name)
+        console.log("hello " + name)
     }
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs)
+    }
+
+    useEffect( () => {
+        console.log("use effect ran");
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
@@ -20,6 +38,9 @@ const Home = () => {
             <p>{name}</p>
             <button onClick={hello}>Click me</button>
             <button onClick={() => helloinput("jack")}> Click me 2</button>
+            <p> ------- </p>
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blog"/>
         </div>
     );
 }
