1e7f31965a00308e38aa55b4667a1808bd46cff8
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-07-17 08:25:43 -0400

    added useFetch custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
     //useState hook syntax. Making a variable reactive
     const [name, setName] = useState("mario");
-    const [blogs, setBlogs] = useState([]);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
 
     const hello = () => {
         console.log("hello from console");
@@ -18,28 +17,12 @@ const Home = () => {
         console.log("hello " + name)
     }
 
-    const handleDelete = (id) => {
+    /* const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs)
-    }
+    } */
 
-    useEffect(() => {
-        fetch("http://localhost:8000/blogss")
-            .then((res) => {
-                if(!res.ok){
-                    throw Error("could not fetch the resource");
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-            })
-            .catch((err) =>{
-                setIsPending(false);
-                setError(err.message);
-            })
-    }, []);
+    
 
     return (
         <div className="home">
@@ -50,7 +33,7 @@ const Home = () => {
             <button onClick={hello}>Click me</button>
             <button onClick={() => helloinput("jack")}> Click me 2</button>
             <p> ------- </p>
-            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+            {<BlogList blogs={blogs} title="All blogs"/>}
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blog" />
         </div>
     );
---
+++ src/useFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then((res) => {
+                if(!res.ok){
+                    throw Error("could not fetch the resource");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setData(data);
+                setIsPending(false);
+            })
+            .catch((err) =>{
+                setIsPending(false);
+                setError(err.message);
+            })
+    }, [url]);
+    return {data, isPending, error}
+}
+
+export default useFetch
