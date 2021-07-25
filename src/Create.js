91a19645b19e72178c1c0928fb346faea1566be4
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-07-25 14:09:10 -0400

    started on create blog component

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState } from "react"
 
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+    }
+
     return (
        <div className="create">
            <h2>Add a new blog</h2>
+           <form onSubmit = {handleSubmit}>
+           <label>Blog Title</label>
+           <input 
+           type="text"
+           required
+           value = { title }
+           onChange = { (e) => setTitle(e.target.value) }/>
+           <label>Blog Body</label>
+           <textarea
+           required
+           value = { body }
+           onChange = { (e) => setBody(e.target.value)}></textarea>
+           <label>Blog Author</label>
+           <select
+           value = { author }
+           onChange = { (e) => setAuthor(e.target.value)}>
+               <option value="Mario">Mario</option>
+               <option value="Luigi">Luigi</option>
+           </select>
+           <button>Add blog</button>
+           <p> { title }</p>
+           <p>{ body }</p>
+           <p> {author}</p>
+           </form>
        </div>
     )
 }
